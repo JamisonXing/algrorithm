5b7d0cbf7d940becc3e16379311783c6aab25631
jamison <2810947907@qq.com>
2022-12-11 19:42:39 +0800

    快速幂

---
+++ src/com/jamison/jianzhi_offer/day19/MyPow.java
@@ -0,0 +1,27 @@
+package com.jamison.jianzhi_offer.day19;
+
+/**
+ * 快速幂，没怎么看懂
+ * <a href="https://leetcode.cn/problems/shu-zhi-de-zheng-shu-ci-fang-lcof/solution/mian-shi-ti-16-shu-zhi-de-zheng-shu-ci-fang-kuai-s/">...</a>
+ */
+class MyPow {
+    public double myPow(double x, int n) {
+        if(x == 0) {
+            return 0;
+        }
+        long b = n;
+        double res = 1.0;
+        if(b < 0) {
+            x = 1 / x;
+            b = -b;
+        }
+        while(b > 0) {
+            if((b & 1) == 1) {
+                res *= x;
+            }
+            x *= x;
+            b >>= 1;
+        }
+        return res;
+    }
+}
