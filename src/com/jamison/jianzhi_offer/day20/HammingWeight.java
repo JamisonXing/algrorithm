946d8664e4b99ccb0746514b85436a84a7f986d2
jamison <2810947907@qq.com>
2022-12-16 19:11:00 +0800

    HammingWeight method_1 逐位判断

---
+++ src/com/jamison/jianzhi_offer/day20/HammingWeight.java
@@ -0,0 +1,20 @@
+package com.jamison.jianzhi_offer.day20;
+
+/**
+ * @author jamison
+ * <a href="https://leetcode.cn/problems/er-jin-zhi-zhong-1de-ge-shu-lcof/solution/mian-shi-ti-15-er-jin-zhi-zhong-1de-ge-shu-wei-yun/">...</a>
+ */
+public class HammingWeight {
+    /**
+     * you need to treat n as an unsigned value
+     */
+    public int hammingWeight(int n) {
+        //逐位判断
+        int res = 0;
+        while(n != 0) {
+            res += n & 1;
+            n >>>= 1;
+        }
+        return res;
+    }
+}
