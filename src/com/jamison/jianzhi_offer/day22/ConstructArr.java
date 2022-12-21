0c3f2255940445de15ddc3e369cb3bbdacd965d7
jamison <2810947907@qq.com>
2022-12-21 16:02:09 +0800

    constructArr

---
+++ src/com/jamison/jianzhi_offer/day22/ConstructArr.java
@@ -0,0 +1,24 @@
+package com.jamison.jianzhi_offer.day22;
+
+/**
+ * <a href="https://leetcode.cn/problems/gou-jian-cheng-ji-shu-zu-lcof/solution/mian-shi-ti-66-gou-jian-cheng-ji-shu-zu-biao-ge-fe/">...</a>
+ */
+class ConstructArr {
+    public int[] constructArr(int[] a) {
+        int len = a.length;
+        if(len == 0) {
+            return new int[0];
+        }
+        int[] b = new int[len];
+        b[0] = 1;
+        int tmp = 1;
+        for(int i = 1; i < len; i++) {
+            b[i] = b[i - 1] * a[i - 1];
+        }
+        for(int i = len - 2; i >= 0; i--) {
+            tmp *= a[i + 1];
+            b[i] *= tmp;
+        }
+        return b;
+    }
+}
