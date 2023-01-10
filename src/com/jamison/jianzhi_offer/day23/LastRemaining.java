42a3ea62deb9fc9b4186a75e5b57815da38131df
jamison <2810947907@qq.com>
2023-01-10 10:15:14 +0800

    lastRemaining

---
+++ src/com/jamison/jianzhi_offer/day23/LastRemaining.java
@@ -0,0 +1,19 @@
+package com.jamison.jianzhi_offer.day23;
+
+import java.util.ArrayList;
+
+class LastRemaining {
+    public int lastRemaining(int n, int m) {
+        ArrayList<Integer> list = new ArrayList<>(n);
+        for(int i = 0; i < n; i++) {
+            list.add(i);
+        }
+        int idx = 0;
+        while(n > 1) {
+            idx = (idx + m - 1) % n;
+            list.remove(idx);
+            n--;
+        }
+        return list.get(0);
+    }
+}
