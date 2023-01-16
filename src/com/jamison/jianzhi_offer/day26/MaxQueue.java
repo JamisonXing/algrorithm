e4a1b1f02594d473d566c13466d2a17ed967904c
jamison <2810947907@qq.com>
2023-01-16 10:53:46 +0800

    MaxQueue

---
+++ src/com/jamison/jianzhi_offer/day26/MaxQueue.java
@@ -0,0 +1,47 @@
+package com.jamison.jianzhi_offer.day26;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * <a href="https://leetcode.cn/problems/dui-lie-de-zui-da-zhi-lcof/solution/jian-zhi-offer-59-ii-dui-lie-de-zui-da-z-0pap/">...</a>
+ */
+class MaxQueue {
+    Queue<Integer> queue;
+    Deque<Integer> deque;
+    public MaxQueue() {
+        queue = new LinkedList<>();
+        deque = new LinkedList<>();
+    }
+
+    public int max_value() {
+        return deque.isEmpty() ? -1 : deque.peekFirst();
+    }
+
+    public void push_back(int value) {
+        queue.offer(value);
+        while(!deque.isEmpty() && deque.peekLast() < value) {
+            deque.pollLast();
+        }
+        deque.offerLast(value);
+    }
+
+    public int pop_front() {
+        if(queue.isEmpty()) {
+            return -1;
+        }
+        if(queue.peek().equals(deque.peekFirst())) {
+            deque.pollFirst();
+        }
+        return queue.poll();
+    }
+}
+
+/**
+ * Your MaxQueue object will be instantiated and called as such:
+ * MaxQueue obj = new MaxQueue();
+ * int param_1 = obj.max_value();
+ * obj.push_back(value);
+ * int param_3 = obj.pop_front();
+ */
